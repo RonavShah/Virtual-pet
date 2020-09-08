eec3a8aef5789d4986071adad61d092ae38f6181
RonavShah <67420914+RonavShah@users.noreply.github.com>
2020-09-08 11:39:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+//Create variables here
+var dog,happyDog,database,food,foodStock;
+
+
+
+function preload()
+{
+  //load images here
+  dog.loadImage("images/dogImg.png");
+  happyDod.loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500,500);
+  
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
